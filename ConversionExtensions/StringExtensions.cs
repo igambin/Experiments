196f919f44d37cd5b628c8767832e25fd5561139
Ingo Gambin <ingo.gambin@gmx.de>
2017-02-17 13:23:44 +0100

    Update StringExtensions.cs

--- ConversionExtensions/StringExtensions.cs
+++ ConversionExtensions/StringExtensions.cs
@@ -6,6 +6,7 @@ namespace IG.Extensions
     {
 
         public static byte[] ToByteArray(this string str) => new UTF8Encoding().GetBytes(str);
+        public static byte[] ToByteArray<TEncoding>(this string str) where TEncoding : Encoding, new() => new TEncoding().GetBytes(str);
 
     }
 }
